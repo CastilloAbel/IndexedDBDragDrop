3d31e6c49b44e9678188e527596b143c4ef5bd0c
Castillo Abel <castillokev.abel@gmail.com>
2022-01-14 12:06:48 -0300

    Css listo, falta js e interacciones

---
+++ codigo.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input");
+const button = document.getElementById("submit");
+const persons = document.querySelector(".persons");
+const fragment = document.createDocumentFragment();
+const img = document.querySelector(".image");
+var personas = 0;
+button.addEventListener("click", ()=>{
+    const person = document.createElement("DIV");
+    person.classList.add("person", `person${personas}`)
+    const htmlCode  = `
+    <input class="names name${personas}" readonly="true" value="${input.value}" type="text">
+    <input class="modify modify${personas}" type="submit" value="Modify">
+    <input class="delete delete${personas}" type="submit" value="Delete">`;
+    person.innerHTML = htmlCode;
+    fragment.appendChild(person);
+    persons.appendChild(fragment);
+    personas++;
+});
+if (personas > 0){
+    const del = document.querySelector("delete")
+    del.addEventListener("dragover", e=>{
+        e.preventDefault();
+        console.log("se esta moviendo")
+    })
+    img.addEventListener("drop", e=>{
+
+    })
+}
